f0fb4e5616ce59a85dbc8385b542754843519762
Aerial <matas234@gmail.com>
2019-11-17 21:11:36 +0100

    test

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Chattr'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://dry-bastion-40520.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
