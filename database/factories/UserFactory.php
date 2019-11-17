92d58704ee61b9fe312191c8d4ef6047937dac73
Aerial <matas234@gmail.com>
2019-11-17 21:21:50 +0100

    test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'avatar' => 'https://api.adorable.io/avatars/150',
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         'remember_token' => Str::random(10),
     ];
 });
