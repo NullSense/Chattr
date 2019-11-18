be12e7a387e87ffa4b0318e1cf18f8f7b108fea3
Aerial <matas234@gmail.com>
2019-11-18 16:23:14 +0100

    Add User import

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Message;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Message::class, function (Faker $faker) {
